ca3ba8394fb4ec389de19d76bce92444c7983d03
Khanh linh LE <khle@made-f0Br2s5.clusters.42paris.fr>
2023-09-07 05:58:12 +0200

    add annonce

--- CPP01/ex01/sources/Zombie.cpp
+++ CPP01/ex01/sources/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: khle <khle@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 02:05:46 by khle              #+#    #+#             */
-/*   Updated: 2023/09/07 05:04:46 by khle             ###   ########.fr       */
+/*   Updated: 2023/09/07 05:54:16 by khle             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 Zombie::Zombie()
 {
 	this->name = "Random";
+	std::cout << "Zombie " << this->name << " has creadted" << std::endl;
+
 }
 
 Zombie::~Zombie()
 {
+	std::cout << "Zombie " << this->name << " has destroyed" << std::endl;
 }
 
 void	Zombie::setname(std::string name)
